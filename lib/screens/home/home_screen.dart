6171ea024fcd63de457b4fc36d9f6612463aea9c
Sahil Bhosale <sahilbhosale@gmail.com>
2025-07-18 14:54:58 -0400

    minor fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_profile/screens/home/components/home_banner.dart';
 import 'package:flutter_profile/screens/home/components/my_projects.dart';
-import 'package:flutter_profile/screens/home/components/recommendations.dart';
 import 'package:flutter_profile/screens/main/main_screen.dart';
 
 import 'components/highlight_info.dart';
